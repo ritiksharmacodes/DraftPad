de007811a8251976627a05d33d322e70ba23a6be
ritiksharmacodes <ritikcan18@gmail.com>
2026-06-01 09:43:02 +0530

    react migration begins

---
+++ draftpad-react-frontend/src/components/headers/main_interface_header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+		<div className="navigation-buttons"><button id="navigation-buttons-backBtn">Back</button></div>
+		
+	</div>
+  )
+}
+
+export default Header
---
+++ draftpad-react-frontend/src/components/headers/main_interface_header/button/Button.tsx
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+function Button() {
+    return (
+        <button id="logout-btn">{}</button>
+    )
+}
+
+export default Button
